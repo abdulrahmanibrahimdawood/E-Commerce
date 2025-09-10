cb186969eaaa5640a52b6ca1dcf14577db1ed807
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-09-10 22:05:14 +0300

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class ECommerce extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Cairo'),
       localizationsDelegates: [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
