6ea40703aa60d60e7d3852dc3f0ac0bb2f58bcfd
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-09-10 15:48:22 +0300

    dots indicator & custom button

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primeryColor = Color(0xff1F7FC1);
+}
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,3 +1,7 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:e_commerce/constants.dart';
+import 'package:e_commerce/core/utils/app_colors.dart';
+import 'package:e_commerce/core/widgets/custom_button.dart';
 import 'package:e_commerce/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +10,25 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPageView()),
+
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primeryColor,
+            color: AppColors.primeryColor.withValues(alpha: 0.5),
+          ),
+        ),
+        SizedBox(height: 29),
+
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: CustomButton(onPressed: () {}, text: "ابدأ الان"),
+        ),
+        SizedBox(height: 43),
+      ],
+    );
   }
 }
