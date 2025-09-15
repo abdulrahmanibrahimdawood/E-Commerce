f26598b9f30061cfb4d5a86b5fd5b9269d14357d
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-09-15 14:05:50 +0300

    Integrate signup cubit

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -2,7 +2,7 @@ import 'package:e_commerce/core/services/get_it_services.dart';
 import 'package:e_commerce/core/widgets/custom_app_bar.dart';
 import 'package:e_commerce/features/auth/domain/repos/auth_repo.dart';
 import 'package:e_commerce/features/auth/presentation/managers/signup_cubit/signup_cubit.dart';
-import 'package:e_commerce/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:e_commerce/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,7 +15,7 @@ class SignupView extends StatelessWidget {
       create: (context) => SignupCubit(getIt<AuthRepo>()),
       child: Scaffold(
         appBar: customAppBar(context, text: 'حساب جديد'),
-        body: SignupViewBody(),
+        body: SignupViewBodyBlocConsumer(),
       ),
     );
   }
