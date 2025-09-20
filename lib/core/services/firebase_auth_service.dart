7bbfb51185f8eade7f60321edf787a7149dbc81a
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-09-20 15:32:10 +0300

    log in with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -37,4 +37,43 @@ class FirebaseAuthService {
       );
     }
   }
+
+  Future<User> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log(
+        'Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}',
+      );
+      if (e.code == 'user-not-found') {
+        throw CustomException(
+          message: 'الرقم السري او البريد الالكتروني غير صحيح.',
+        );
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+          message: 'الرقم السري او البريد الالكتروني غير صحيح.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'لا يوجد اتصال بالانترنت.');
+      } else {
+        throw CustomException(
+          message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+        );
+      }
+    } catch (e) {
+      log(
+        'Exception in FirebaseAuthService.signInWithEmailAndPassword: ${e.toString()}',
+      );
+    }
+    throw CustomException(
+      message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+    );
+  }
 }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -34,4 +34,26 @@ class AuthRepoImpl extends AuthRepo {
       return left(ServerFailure('حدث خطأ ما. الرجاء المحاولة مرة اخرى.'));
     }
   }
+
+  @override
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    try {
+      final user = await firebaseAuthService.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return left(ServerFailure(e.message));
+    } catch (e) {
+      log(
+        'Exception in AuthRepoImpl.signInWithEmailAndPassword: ${e.toString()}',
+      );
+      return left(ServerFailure('حدث خطأ ما. الرجاء المحاولة مرة اخرى.'));
+    }
+  }
 }
