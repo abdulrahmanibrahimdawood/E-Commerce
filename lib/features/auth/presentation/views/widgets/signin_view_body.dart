8fc37073ff396d9669b6f9a038729286b47fbd3b
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-09-21 14:49:44 +0300

    sign in with email and password

--- lib/features/auth/presentation/views/widgets/signin_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body.dart
@@ -3,70 +3,102 @@ import 'package:e_commerce/core/utils/app_colors.dart';
 import 'package:e_commerce/core/utils/app_images.dart';
 import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
+import 'package:e_commerce/core/widgets/custom_password_field.dart';
 import 'package:e_commerce/core/widgets/custom_text_form_field.dart';
+import 'package:e_commerce/features/auth/presentation/managers/signin_cubit/signin_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/views/widgets/dont_have_an%20_account_widget.dart';
 import 'package:e_commerce/features/auth/presentation/views/widgets/or_divider.dart';
 import 'package:e_commerce/features/auth/presentation/views/widgets/social_login_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SigninViewBody extends StatelessWidget {
+class SigninViewBody extends StatefulWidget {
   const SigninViewBody({super.key});
+
+  @override
+  State<SigninViewBody> createState() => _SigninViewBodyState();
+}
+
+class _SigninViewBodyState extends State<SigninViewBody> {
+  late String email, password;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            CustomTextFormField(
-              hintText: 'البريد الإلكتروني',
-              textInputType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16),
-            CustomTextFormField(
-              suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
-              hintText: 'كلمة المرور',
-              textInputType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  " نسيت كلمة المرور؟",
-                  style: TextStyles.bold13.copyWith(
-                    color: AppColors.lightPrimaryColor,
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              CustomTextFormField(
+                onSaved: (value) {
+                  email = value!;
+                },
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 16),
+              PasswordField(
+                onSaved: (value) {
+                  password = value!;
+                },
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    " نسيت كلمة المرور؟",
+                    style: TextStyles.bold13.copyWith(
+                      color: AppColors.lightPrimaryColor,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 33),
-            CustomButton(onPressed: () {}, text: 'تسجيل دخول'),
-            SizedBox(height: 33),
-            DontHaveAnAccountWidget(),
-            SizedBox(height: 33),
-            OrDivider(),
-            SizedBox(height: 33),
-            SocialLoginButton(
-              onPressed: () {},
-              image: Assets.assetsImagesGoogleIcon,
-              title: 'تسجيل بواسطة جوجل ',
-            ),
-            SizedBox(height: 16),
-            SocialLoginButton(
-              onPressed: () {},
-              image: Assets.assetsImagesAppleIcon,
-              title: 'تسجيل بواسطة أبل ',
-            ),
-            SizedBox(height: 16),
-            SocialLoginButton(
-              onPressed: () {},
-              image: Assets.assetsImagesFacebookIcon,
-              title: 'تسجيل بواسطة فيسبوك ',
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 33),
+              CustomButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    context.read<SigninCubit>().signin(email, password);
+                  } else {
+                    {
+                      autovalidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  }
+                },
+                text: 'تسجيل دخول',
+              ),
+              SizedBox(height: 33),
+              DontHaveAnAccountWidget(),
+              SizedBox(height: 33),
+              OrDivider(),
+              SizedBox(height: 33),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesGoogleIcon,
+                title: 'تسجيل بواسطة جوجل ',
+              ),
+              SizedBox(height: 16),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesAppleIcon,
+                title: 'تسجيل بواسطة أبل ',
+              ),
+              SizedBox(height: 16),
+              SocialLoginButton(
+                onPressed: () {},
+                image: Assets.assetsImagesFacebookIcon,
+                title: 'تسجيل بواسطة فيسبوك ',
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce/core/helper_funcations/build_error_bar.dart'
+    show buildErrorBar;
 import 'package:e_commerce/core/widgets/custom_progress_hud.dart';
 import 'package:e_commerce/features/auth/presentation/managers/signin_cubit/signin_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/views/widgets/signin_view_body.dart';
@@ -10,7 +12,12 @@ class SigninViewBodyBlocConsumer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SigninCubit, SigninState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is SigninSuccess) {}
+        if (state is SigninFailure) {
+          buildErrorBar(context, state.message);
+        }
+      },
       builder: (context, state) {
         return CustomProgressHUD(
           isLoading: state is SigninLoading ? true : false,
