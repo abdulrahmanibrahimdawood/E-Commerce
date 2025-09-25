350a1a7dee3dd190086b6e84c93e3ebb89986c65
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-09-25 18:19:32 +0300

    implement add data method

--- lib/core/services/firestore_services.dart
+++ lib/core/services/firestore_services.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce/core/services/data_service.dart';
 
 class FirestoreServices implements DatabaseServices {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   @override
+  //هنا عمللت دي عشان اقدر اضيف داتا بشكل عام للداتا بيز بتاعتنا
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
-  }) {
-    // TODO: implement addData
-    throw UnimplementedError();
+  }) async {
+    await firestore.collection(path).add(data);
   }
 }
