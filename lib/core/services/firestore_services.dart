32bfcaf02432d62a3d8cfa9f4b882c8ac9a7ff3b
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-09-25 16:11:45 +0300

    create data service and abstract firestore service

---
+++ lib/core/services/firestore_services.dart
@@ -0,0 +1,12 @@
+import 'package:e_commerce/core/services/data_service.dart';
+
+class FirestoreServices implements DatabaseServices {
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
