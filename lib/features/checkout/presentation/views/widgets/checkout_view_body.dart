cd0b314b3717368a4d27259778680ddb500ed608
Abdulrahman Dawoud <kabdo2223@gmail.com>
2025-11-07 21:51:53 +0200

    add payment paypal package

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -6,6 +6,7 @@ import 'package:e_commerce/features/checkout/presentation/manager/add_order_cubi
 import 'package:e_commerce/features/checkout/presentation/views/widgets/checkout_page_view.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/checkout_steps.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:provider/provider.dart';
 
 class CheckoutViewBody extends StatefulWidget {
@@ -115,4 +116,73 @@ String getNextButtonText(int currentPageIndex) {
     default:
       return 'التالي';
   }
+}
+
+void processPayment(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (BuildContext context) => PaypalCheckoutView(
+        sandboxMode: true,
+        clientId: "",
+        secretKey: "",
+        transactions: const [
+          {
+            "amount": {
+              "total": '70',
+              "currency": "USD",
+              "details": {
+                "subtotal": '70',
+                "shipping": '0',
+                "shipping_discount": 0,
+              },
+            },
+            "description": "The payment transaction description.",
+            // "payment_options": {
+            //   "allowed_payment_method":
+            //       "INSTANT_FUNDING_SOURCE"
+            // },
+            "item_list": {
+              "items": [
+                {
+                  "name": "Apple",
+                  "quantity": 4,
+                  "price": '5',
+                  "currency": "USD",
+                },
+                {
+                  "name": "Pineapple",
+                  "quantity": 5,
+                  "price": '10',
+                  "currency": "USD",
+                },
+              ],
+
+              // shipping address is not required though
+              //   "shipping_address": {
+              //     "recipient_name": "tharwat",
+              //     "line1": "Alexandria",
+              //     "line2": "",
+              //     "city": "Alexandria",
+              //     "country_code": "EG",
+              //     "postal_code": "21505",
+              //     "phone": "+00000000",
+              //     "state": "Alexandria"
+              //  },
+            },
+          },
+        ],
+        note: "Contact us for any questions on your order.",
+        onSuccess: (Map params) async {
+          print("onSuccess: $params");
+        },
+        onError: (error) {
+          print("onError: $error");
+          Navigator.pop(context);
+        },
+        onCancel: () {
+          print('cancelled:');
+        },
+      ),
+    ),
+  );
 }
